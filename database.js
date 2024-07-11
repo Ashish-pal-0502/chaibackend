4e856130d3a9e921ba14c6f757344391c03c5c2d
Ashish Pal <ashishpal0502@gmail.com>
2024-07-11 21:03:20 +0530

    database

--- database.js
+++ database.js
@@ -1,7 +1,9 @@
+require("dotenv").config();
+
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://localhost:27017/register")
+  .connect(process.env.DATABASE_URI)
   .then(() => {
     console.log("database connected successfully");
   })
